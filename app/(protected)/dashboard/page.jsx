f295319c55d4702c5cc50b61a31ee7dd185c29d1
coder-mahmud <mahmud.linked@gmail.com>
2025-04-06 22:20:12 +0600

    Added loader

--- app/(protected)/dashboard/page.jsx
+++ app/(protected)/dashboard/page.jsx
@@ -4,10 +4,12 @@ import { useSelector } from 'react-redux';
 import { redirect } from 'next/navigation';
 import { useEffect } from 'react';
 import useUserBootstrap from '@/hooks/useUserBootstrap';
+import Loader from '@/components/Loader';
 
 export default function DashboardPage() {
   const { loading } = useUserBootstrap();
   const { user } = useSelector((state) => state.auth);
+  const [showLoader, setShowLoader] = useState(true)
   
 
 
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import Loader from "@/components/Loader";
 
 export default function Home() {
   return (
     <div className="">
+      {/* <Loader /> */}
       <h1>Hi there!</h1>
     </div>
   );
